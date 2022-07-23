da3f3f353f6275aee1a6298ecf457ea6f95cd156
lhs7091 <lhs7091@naver.com>
2022-07-23 11:32:46 +0900

    config server 추가

--- ecommerce-java/user-service/src/main/java/com/ecommerce/userservice/controller/UserController.java
+++ ecommerce-java/user-service/src/main/java/com/ecommerce/userservice/controller/UserController.java
@@ -24,7 +24,12 @@ public class UserController {
 
     @GetMapping("/health_check")
     public String status(){
-        return String.format("It's Working in User Service on Port %s", env.getProperty("local.server.port"));
+//        return String.format("It's Working in User Service on Port %s", env.getProperty("local.server.port"));
+        return String.format("It's Working in User service" +
+                ", port(local.server.port) = "+env.getProperty("local.server.port") +
+                ", port(server.port) = "+env.getProperty("server.port") +
+                ", token sercret = "+env.getProperty("token.secret") +
+                ", token expiration time = "+env.getProperty("token.expiration_time"));
     }
 
     @GetMapping("/welcome")
